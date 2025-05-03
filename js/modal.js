b7627c208eec1b1cf316ea373fd39d4915d9554e
Артём Чеснов <88346227+ArtemChesnov@users.noreply.github.com>
2025-05-03 14:42:56 +0300

    Delete js directory

--- js/modal.js
+++
@@ -1,7 +0,0 @@
-function openModal(event) {
-  document.querySelector(".application__overlay").classList.add("active");
-}
-
-function closeModal(event) {
-  document.querySelector(".application__overlay").classList.remove("active");
-}
