1cbb4ba8f46756567a9c9d52d28845907948c533
Артём Чеснов <88346227+ArtemChesnov@users.noreply.github.com>
2025-05-21 22:28:39 +0300

    fixed some

--- mail_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'host' => 'smtp.mail.ru',
-    'username' => 'mailer.info.zenit.nn@mail.ru',
-    'password' => 'Rm9AJi2Zm1LQrRdUg3uu',
-    'port' => 465,
-    'secure' => 'ssl'
-];
